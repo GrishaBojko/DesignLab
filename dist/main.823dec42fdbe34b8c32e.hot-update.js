634d94e68102037ab9a02126878f921aeaad0383
GrishaBojko <grishabojko2@gmail.com>
2019-11-08 21:53:20 +0200

    1

---
+++ dist/main.823dec42fdbe34b8c32e.hot-update.js
@@ -0,0 +1,14 @@
+webpackHotUpdate("main",{
+
+/***/ "./node_modules/file-loader/dist/cjs.js?!./node_modules/extract-loader/lib/extractLoader.js!./node_modules/css-loader/dist/cjs.js?!./node_modules/sass-loader/dist/cjs.js?!./node_modules/postcss-loader/src/index.js?!./src/scss/styles.scss":
+/*!*******************************************************************************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/file-loader/dist/cjs.js??ref--8-1!./node_modules/extract-loader/lib/extractLoader.js!./node_modules/css-loader/dist/cjs.js??ref--8-3!./node_modules/sass-loader/dist/cjs.js??ref--8-4!./node_modules/postcss-loader/src??ref--8-5!./src/scss/styles.scss ***!
+  \*******************************************************************************************************************************************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("throw new Error(\"Module build failed (from ./node_modules/postcss-loader/src/index.js):\\nSyntaxError\\n\\n(776:5) Unknown word\\n\\n \\u001b[90m 774 | \\u001b[39m  \\u001b[33m.header__content\\u001b[39m\\u001b[33m{\\u001b[39m\\n \\u001b[90m 775 | \\u001b[39m    height\\u001b[33m:\\u001b[39m 60vh\\u001b[33m;\\u001b[39m\\n\\u001b[31m\\u001b[1m>\\u001b[22m\\u001b[39m\\u001b[90m 776 | \\u001b[39m    js\\n \\u001b[90m     | \\u001b[39m    \\u001b[31m\\u001b[1m^\\u001b[22m\\u001b[39m\\n \\u001b[90m 777 | \\u001b[39m  \\u001b[33m}\\u001b[39m\\n \\u001b[90m 778 | \\u001b[39m  \\u001b[33m.header__logo\\u001b[39m\\u001b[33m{\\u001b[39m\\n\");\n\n//# sourceURL=webpack:///./src/scss/styles.scss?./node_modules/file-loader/dist/cjs.js??ref--8-1!./node_modules/extract-loader/lib/extractLoader.js!./node_modules/css-loader/dist/cjs.js??ref--8-3!./node_modules/sass-loader/dist/cjs.js??ref--8-4!./node_modules/postcss-loader/src??ref--8-5");
+
+/***/ })
+
+})
